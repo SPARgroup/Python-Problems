7b1224f6782bb625412f0016a5ef840a97b53cc0
Samarth Singla <25768584+samarthsingla@users.noreply.github.com>
2019-05-09 14:52:32 +0530

    Version 1 completed! And glitch fixed...

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,13 +2,13 @@ import os
 import msvcrt as ms
 import time
 
-
+# A simple TicTacToe game
 def display(currState):
     os.system("cls")
     for i in range(3):
         for j in range(3):
             print(currState[(i * 3) + j], end="")
-            if (j+1) % 3 != 0:
+            if (j + 1) % 3 != 0:
                 print(" | ", end="")
 
         print("\n")
@@ -21,24 +21,47 @@ def process(s):
         return -1
     elif s == "d":
         return 1
-    else:  # s is "s"
+    elif s == "s":  # s is "s"
         return 3
+    else:
+        return False
+
+
+def check(z):
+    x = len(z)
+    for i in range(0, x - 2):
+        for j in range(i + 1, x - 1):
+            for k in range(j + 1, x):
+                if z[i] + z[j] + z[k] == 15:
+                    return True
+
+    return False
 
 
 board = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
 config = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
 
-magic = [6, 7, 2, 1, 5, 9, 8, 3, 4]
+magic = [6, 7, 2,
+         1, 5, 9,
+         8, 3, 4]
 
 X = []
 Y = []
+
 turn = True
+won = False
+
 moves = 0
-while moves < 9:
+
+while moves <= 9 and not won:
     moves += 1
-    counter = 0
+
+    counter = 0  #the starting point is the top-left. Change to 5 to start at center.
+
     display(board)
+
     insertVal = ""
+
     if turn:
         print("Player X")
         insertVal = "X"
@@ -46,27 +69,62 @@ while moves < 9:
         print("Player O")
         insertVal = "O"
 
-
     print("Play Your Move using WASD keys: ")
-    x = input()  #string for input processing
 
-    for j in x:
+    x = input()
+
+    for j in x:  #processing keypresses
         move = process(j)
+        if move == False:
+            print("Hold up!")
+            time.sleep(0.5)
+            counter = -1
+            break
         counter = (counter + move) % 9
 
-
-    if board[counter] != "_":
-        print("Invalid Move")
+    if board[counter] != "_" or counter < 0:
+        print("Wait.")
         moves -= 1
+        time.sleep(0.5)
+        print("That's illegal!")
+        print(insertVal)
         time.sleep(1)
     else:
         board[counter] = insertVal
         if turn:
+            turn = False
             X.append(magic[counter])
+            if moves >= 5:
+                if check(X):
+                    display(board)
+                    print("Outstanding move!")
+                    won = True
+                    time.sleep(0.4)
+                    print("Player X Wins.")
+                else:
+                    continue
+
         else:
+            turn = True
             Y.append(magic[counter])
+            if moves > 5:
+                if check(Y):
+                    display(board)
+                    print("Outstanding move!")
+                    won = True
+                    time.sleep(0.4)
+                    print("Player Y Wins.")
+                else:
+                    continue
 
-        turn = not turn
+    if moves == 9:
+        break
 
+if not won:
+  print("Nobody won and its a shame.\n")
+inp = ""
+while(not(inp == "F" or inp == "f")):
+  inp = input("\nPress F and Enter to pay respects. ")
 
-input()
+print("Endgame Now")
+time.sleep(1)
