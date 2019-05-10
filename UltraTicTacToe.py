baf9c420c0de221008bb989b9c4318e9f72a20f0
Samarth Singla <25768584+samarthsingla@users.noreply.github.com>
2019-05-10 20:45:12 +0530

    UltraTicTacToe local functionality done. Cheers!

--- UltraTicTacToe.py
+++ UltraTicTacToe.py
@@ -34,13 +34,25 @@ magic = [6, 7, 2,
          8, 3, 4]
 
 
-class player:
-    ac = [[], [], [], [], [], [], [], [], []]
-    wins = []
+
+class player():
+    def __init__(self):
+        self.ac = [[], [], [], [], [], [], [], [], []]
+        self.wins = [False, False, False, False, False, False, False, False, False]
+        self.moves = [0,0,0,0,0,0,0,0,0]
 
 
 x = player()
 y = player()
+
+
+def win(checkList):
+    counter = 0
+    for i in checkList:
+        if i == True:
+            counter += 1
+
+    return counter
 
 
 def display_row(currState, jay, kay):
@@ -64,14 +76,125 @@ def ultrashow(currState):
                 print("_", end="")
         print("\n")
 
-#@todo: find Python API to use network to store game states at a network location
-moves = 1
-bigscope = 0
-while moves <= 81:
-    smallscope = 0
+def process(s):
+    if s == "w":
+        return -3
+    elif s == "a":
+        return -1
+    elif s == "d":
+        return 1
+    elif s == "s":  # s is "s"
+        return 3
+    else:
+        return False
 
-    #bigscope = smallscope
+def check(z):
+    x = len(z)
+    for i in range(0, x - 2):
+        for j in range(i + 1, x - 1):
+            for k in range(j + 1, x):
+                if z[i] + z[j] + z[k] == 15:
+                    return True
+
+    return False
+
+def isfilled(l):
+    for i in l:
+        if i == '_':
+            return False
+    else:
+        return True
+
+#@todo: find Python API to use network to store game states at a network location
+
+moves = 1
+
+bigscope = 0
+turn = True
+
+while moves <= 81:
+    smallscope = 4
+
+    ultrashow(board)
+
+    insertVal = ""
+
+    if turn:
+        insertVal="X"
+    else:
+        insertVal="O"
+
+    # if(win(x.wins) > 0 or win(y.wins) > 0):
+    #     print("X :",win(x.wins),"Y :",win(y.wins))
+
+    print("X :", win(x.wins), "Y :", win(y.wins))
+    print("\nIt is player",insertVal+"'s turn. You will play in the",bigscope+1,"grid.")
+    print("\nNavigate using WASD keys (your pointer starts at the center) :")
+
+    inp = input()
+
+    for i in inp:
+        move = process(i)
+        if move == False:
+            print("Hold up!")
+            time.sleep(0.5)
+            smallscope = -1
+            break
+        else:
+            smallscope = (smallscope + move) % 9
+
+      #@todo: check if destination bigscope is already filled
+      #@todo: a variable to track how many total wins are there
+
+    if smallscope < 0 or board[bigscope][smallscope] != '_':
+        print("Wait.")
+        time.sleep(0.5)
+        print("That's illegal.")
+        moves =- 1
+        time.sleep(0.5)
+    else:
+        board[bigscope][smallscope] = insertVal
+        if turn:
+            turn = False
+            x.moves[bigscope] += 1
+
+            x.ac[bigscope].append(magic[smallscope])
+
+            if x.moves[bigscope] >= 3:
+
+                if check(x.ac[bigscope]) and y.wins[bigscope] == False:
+                    x.wins[bigscope] = True
+                else:
+                    continue
+
+            if isfilled(board[smallscope]):
+
+                print("They have no space for you in the",smallscope+1,"grid. So might as well be here itself.")
+                time.sleep(3)
+
+            else:
+                bigscope = smallscope
+
+        else:
+            turn = True
+            y.moves[bigscope] += 1
+            y.ac[bigscope].append(magic[smallscope])
+
+            if y.moves[bigscope] >= 3:
+
+                if check(y.ac[bigscope]) and x.wins[bigscope] == False:
+                    y.wins[bigscope] = True
+                else:
+                    continue
+
+            if isfilled(board[smallscope]):
+
+                print("They have no space for you in the",smallscope+1,"grid. So might as well be here itself.")
+                time.sleep(3)
+
+            else:
+                bigscope = smallscope
+
     moves += 1
 
-#ultrashow(board)
 input()
