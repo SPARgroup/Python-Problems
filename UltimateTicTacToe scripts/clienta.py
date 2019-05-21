9f1ae4bc5cbe66597548cc1859b50bb5fc53f315
SPAR <spargroup65@gmail.com>
2019-05-21 11:38:41 +0530

    peer to peer on lan, without threading, without server implemented.

--- UltimateTicTacToe scripts/clienta.py
+++ UltimateTicTacToe scripts/clienta.py
@@ -1,22 +1,28 @@
 import socket as sock
 import time
 
-server_ip = "192.168.2.62"
-server_port = 12334
+target_local = "192.168.88.75"#set manually, server not set up
+target_port = 0
+me = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
 
-#my_ip = "192.168.2.62"
+me.bind(("", 0))
 
-hellosocket = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
-hellosocket.bind(('', 0))
+print("My Port: ", me.getsockname()[1])
 
-print(hellosocket.getsockname()[1])
+#s = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
 
-msg = b"Oh Yeah"
+target_port = int(input("Enter target's port:"))
 
-hellosocket.sendto(msg, (server_ip, server_port))
+#s.bind((target_local, target_port))
 
-data, addr = hellosocket.recvfrom(1024)
-
-print(data.decode("utf-8"), addr)
-
-input()
+while 1:
+    try:
+        msg = input("Enter message for Bheem:").encode("utf-8")
+        me.sendto(msg, (target_local, target_port))
+        data, addr = me.recvfrom(1024)
+        data = data.decode("utf-8")
+        print("Bheem says:", data)
+        time.sleep(2)
+    except:
+        time.sleep(1)
+        pass
