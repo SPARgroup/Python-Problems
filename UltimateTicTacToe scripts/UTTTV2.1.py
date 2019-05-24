7c73bfe59d5caffa35f65f51da0ce35f2dd9ed3b
SPAR <spargroup65@gmail.com>
2019-05-24 12:12:23 +0530

    XMPP started.

--- UltimateTicTacToe scripts/UTTTV2.1.py
+++ UltimateTicTacToe scripts/UTTTV2.1.py
@@ -3,7 +3,44 @@ import os
 import requests as req
 import msvcrt as ms
 import copy
+import ctypes
+import subprocess
+
+
 #Ultimate TICTACTOE v2.1
+#
+# from ctypes import wintypes
+#
+# kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+# user32 = ctypes.WinDLL('user32', use_last_error=True)
+#
+# SW_MAXIMIZE = 3
+#
+# kernel32.GetConsoleWindow.restype = wintypes.HWND
+# kernel32.GetLargestConsoleWindowSize.restype = wintypes._COORD
+# kernel32.GetLargestConsoleWindowSize.argtypes = (wintypes.HANDLE,)
+# user32.ShowWindow.argtypes = (wintypes.HWND, ctypes.c_int)
+#
+# def maximize_console(lines=None):
+#     fd = os.open('CONOUT$', os.O_RDWR)
+#     try:
+#         hCon = ms.get_osfhandle(fd)
+#         max_size = kernel32.GetLargestConsoleWindowSize(hCon)
+#         if max_size.X == 0 and max_size.Y == 0:
+#             raise ctypes.WinError(ctypes.get_last_error())
+#     finally:
+#         os.close(fd)
+#     cols = max_size.X
+#     hWnd = kernel32.GetConsoleWindow()
+#     if cols and hWnd:
+#         if lines is None:
+#             lines = max_size.Y
+#         else:
+#             lines = max(min(lines, 9999), max_size.Y)
+#         subprocess.check_call('mode.com con cols={} lines={}'.format(
+#                                 cols, lines))
+#         user32.ShowWindow(hWnd, SW_MAXIMIZE)
+
 
 board = [['_', '_', '_',
           '_', '_', '_',
