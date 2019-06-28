5624e2c24b2c3b1a0fc24a9989e068dc4142abf4
Samarth Singla <25768584+samarthsingla@users.noreply.github.com>
2019-06-28 20:17:50 +0530

    Added images, started rendering.

--- UltimateTicTacToe-scripts/onlinepvp/globals.py
+++ UltimateTicTacToe-scripts/onlinepvp/globals.py
@@ -1,5 +1,15 @@
 disp = None
 clock = None
 FPS = 60
-size = (600, 600) #window size
+size = (1200, 1200) #window size
 title = "Ultimate TicTacToe Multiplayer v3.0"
+
+boardsize = (600, 600)
+smallsize = (boardsize[0]//3, boardsize[1]//3)
+x_o_size = (smallsize[0]//3, smallsize[1]//3)
+squaresize = x_o_size
+
+pos = [[],[],[],[],[],[],[],[],[]]
+centers = [(0,0)] * 9
+
+board_center_offset = (0,0)
--- UltimateTicTacToe-scripts/onlinepvp/renderer.py
+++ UltimateTicTacToe-scripts/onlinepvp/renderer.py
@@ -1,11 +1,126 @@
 import pygame as pg, pygame
 import globals as g
+import os
+import time
 
+#vars
+board = [['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_'],
+         ['_', '_', '_',
+          '_', '_', '_',
+          '_', '_', '_']]
+#contaiiner for images
+class images:
+    pass
 
+class colors:
+    black = (0,0,0)
+    pass
+
+#initialization function
 def init():
+    global board
     pygame.init()
     g.disp = pygame.display.set_mode(g.size)
-    g.clock = pygame.time.clock(g.FPS)
+    g.clock = pygame.time.Clock()
+    loadimages()
 
-def render(board):
+
+def render(board, bigscope, smallscope):
     pass
+
+def loadimages():
+    images.bigboard = pg.transform.scale(pg.image.load("resources/bigboard.png").convert_alpha(), g.boardsize)
+    images.smallboard = pg.transform.scale(pg.image.load("resources/smallboard.png").convert_alpha(),g.smallsize)
+    images.square = pg.transform.scale(pg.image.load("resources/square.png").convert_alpha(),g.squaresize)
+    images.x = pg.transform.scale(pg.image.load("resources/x.png").convert_alpha(),g.x_o_size)
+    images.o = pg.transform.scale(pg.image.load("resources/o.png").convert_alpha(),g.x_o_size)
+
+#part of calc_positions function
+def row_pos(currState, jay, kay, aai):
+    print(currState[(3 * jay)], end=" | ")
+    print(currState[(3 * jay) + 1], end=" | ")
+    print(currState[(3 * jay) + 2], end="")
+    print(aai, jay,kay,end="")
+
+    #g.pos[(3 * jay)] =
+
+    if kay != 2:
+        print("   I   ", end="") #mid divider
+
+
+
+def calc_centers(currState):
+    #os.system("cls")
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                row_pos(currState[(3 * i) + k], j, k, i)
+
+                g.centers[(3*i) + k] = (int(g.smallsize[0]*(k + 0.5) + g.board_center_offset[0]), int(g.smallsize[1]*(i+0.5) + g.board_center_offset[1]))
+
+            print("\n")
+        if (i != 2):
+            for a in range(44):
+                print("_", end="")
+        print("\n")
+
+def calc_positions():
+    c = 0
+    size = g.squaresize[0] #it has to be square aspect ratio
+    for center in g.centers:
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                x = center[0] + (size * i)
+                y = center[1] + (size * j)
+                g.pos[c].append((x,y))
+
+        c += 1
+
+def sample_show():
+    g.disp.fill(colors.black)
+    g.disp.blit(images.bigboard, (0,0))
+    for each in g.pos:
+        for pos in each:
+            renderAtCenter(images.x, pos)
+
+    pygame.display.update()
+
+def renderAtCenter(img, coord):
+    rect=img.get_rect()
+    rect.center=coord
+    g.disp.blit(img,rect)
+
+
+init()
+calc_centers(board)
+calc_positions()
+sample_show()
+
+while 1:
+    for event in pygame.event.get():
+        pass
+
+print(g.centers)
